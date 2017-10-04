c44ffacaf6844e8fcb1dccaadd78ef87be631a02
tpahuja <tpahuja@ncsu.edu>
2017-10-04 17:42:13 -0400

    Working user

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ApplicationRecord
   has_many :reservation #,  dependent: :destroy
 
   validates :license, presence: true, uniqueness: true
-  validates :status, inclusion: { in: ["checked_out", "available"],  message: 'needs to be either chechout or available' }
+  validates :status, inclusion: { in: ["Checked_out", "Available"],  message: 'needs to be either Checked_out or Available' }
 end
