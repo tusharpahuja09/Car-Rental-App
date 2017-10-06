9e1b7c7a74e4f7f516fd5912f6321fba0adfa3df
tpahuja <tpahuja@ncsu.edu>
2017-10-06 00:16:46 -0400

    tushar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user.create([{email: 'admin@ncsu.edu', password: '123456' ,user_type: '1'  }])
-user.create([{email: 'superadmin@ncsu.edu', password: 'superadminadmin' ,user_type: '2'  }])
-user.create([{email: 'superadmin1@ncsu.edu', password: 'superadminadmin' ,user_type: '2'  }])
+User.create([{email: 'admin@ncsu.edu', password: '123456' ,user_type: '1'  }])
+User.create([{email: 'superadmin@ncsu.edu', encrypted_password: 'superadminadmin' ,user_type: '2'  }])
+User.create([{email: 'superadmin1@ncsu.edu', password: 'superadminadmin' ,user_type: '2'  }])
