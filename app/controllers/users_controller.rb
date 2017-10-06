cc235113e874fdce03e45d1e2d37c578f9453373
tpahuja <tpahuja@ncsu.edu>
2017-10-06 18:31:51 -0400

    new_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+       @user.save
+
+
     end
   end
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :reservations
   end
   devise_for :users
+  #resources :users, except: :create
+  #post 'create_user' => 'users#create', as: :create_user
   resources :users, :only => [:index, :show, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/car_find_display'=> 'cars#display'
